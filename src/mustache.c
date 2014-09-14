5522c6510bb07725a813f86f35c9eeed175ce153
AntonioCS <antoniocs@gmail.com>
2014-09-14 22:43:12 +0100

    Added function to initialize text_parsed. Added function to escape text (currently public for a testing in main.c) . Added section to check witch type of tag

--- src/mustache.c
+++ src/mustache.c
@@ -22,8 +22,11 @@ static bool tag_end_last(pmustache, char *);
 //All of the k* functions will call this one
 static bool tag_char(char *, pmustache, char *);
 
+static bool text_parsed_init(pmustache);
 static void text_parsed_add_char(pmustache, char *);
 static void text_parsed_add_string(pmustache m, char *);
+
+//static char *text_escape(char *);
 
 pmustache mustache_init() {
     pmustache m = malloc(sizeof (mustache));
@@ -53,22 +56,64 @@ pmustache mustache_init() {
 }
 
 void mustache_render(pmustache m) {
-    char c;    
+    char c;
+    bool escape_values = true;
+
+    if (text_parsed_init(m) != true) {
+        printf("Unable to initialize text_parsed\n");
+        exit(-1);
+    }
 
     while ((c = text_get_char(m)) != EOF) {
 
+        //check if this is the start of a tag
         if (tag_start(m, &c)) {
             char tag_char = text_get_char(m);
-            //check if this is the start of a tag
+
             if (tag_start_last(m, &tag_char)) {
-                //@TODO: Tag type
+                char tag_type = text_get_char(m);
+
+                switch (tag_type) {
+
+                    //do not escape characters
+                    case '{': //will have an extra } as close char
+                        escape_values = false;
+                        break;
+                    //Sections
+                    case '#':
+                        break;
+                    //Inverted Sections
+                    case '^':
+                        break;
+
+                    //Comments
+                    case '!':
+                        break;
+
+                    //Partials (I believe this is basically a way to include other templates)
+                    case '>':
+                        break;
+
+                    //Set Delimiter
+                    case '=':
+                        break;
+
+
+
+                    default:
+                        ;
+                }
+
 
                 //Default value tag
                 char *ptag = calloc(MUSTACHE_TAGS_MAX_LEN_SIZE, sizeof (char));
                 int tag_index = 0;
-                
+
 
                 while ((tag_char = text_get_char(m))) {
+
+                    //if (escape_values == false)
+
                     if (tag_end(m, &tag_char)) {
                         char tmp_char = text_get_char(m);
                         if (tag_end_last(m, &tmp_char)) {
@@ -93,15 +138,14 @@ void mustache_render(pmustache m) {
                         text_parsed_add_string(m, replace_string);
                     }
                 }
-                
+
                 free(ptag);
                 ptag = NULL;
             } else {
                 pos_dec(m);
-                goto add_char;
+                text_parsed_add_char(m, &c);
             }
         } else {
-            add_char:
             text_parsed_add_char(m, &c);
         }
     }
@@ -118,16 +162,16 @@ char *mustache_get(pmustache m, char *key) {
             return m->tags_values[i+1];
         }
     }
-    
+
     return NULL;
 }
 
 void mustache_set(pmustache m, char *key, char *value) {
     m->tags_values[m->tags_values_index] = key;
     m->tags_values_index++;
-    
+
     m->tags_values[m->tags_values_index] = value;
-    m->tags_values_index++;    
+    m->tags_values_index++;
 }
 
 char text_get_char(pmustache m) {
@@ -156,7 +200,7 @@ int pos_get_inc(pmustache m) {
 }
 
 void text_set(pmustache m, char *text) {
-    m->text = text;    
+    m->text = text;
 }
 
 void text_set_size(pmustache m) {
@@ -165,6 +209,16 @@ void text_set_size(pmustache m) {
 
 int text_get_size(pmustache m) {
     return m->text_size;
+}
+
+bool text_parsed_init(pmustache m) {
+    if (m->text_size > 0) {
+        m->text_parsed = calloc(m->text_size *2, sizeof(char));
+
+        return (m->text_parsed != NULL);
+    }
+
+    return false;
 }
 
 void text_parsed_add_char(pmustache m, char *c) {
@@ -216,3 +270,74 @@ bool tag_char(char *key, pmustache m, char *c) {
     return (tag_c == *c);
 }
 
+char *text_escape(char *string) {
+    if (string != NULL) {
+        int len = strlen(string);
+
+        int escape_size = len*2;
+        int escaped_index = 0;
+        char *escaped_text = calloc(escape_size, sizeof(char));
+
+        int index = 0;
+
+        int add = 0;
+        char *to_add = NULL;
+
+        while (string[index] != '\0') {
+
+            switch (string[index]) {
+                case '&':
+                    to_add = "&amp;";
+                    add = 5;
+                    break;
+                case '\"':
+                    to_add =  "&quot;";
+                    add = 6;
+                    break;
+                case '\'':
+                    to_add = "&apos;";
+                    add = 6;
+                    break;
+                case '<':
+                    to_add = "&lt;";
+                    add = 4;
+                break;
+                case '>':
+                    to_add = "&gt;";
+                    add = 4;
+                    break;
+                default:
+                    to_add = &(string[index]);
+                    add = 1;
+            }
+
+            if (add > 1) {
+                if ((escaped_index + add) > escape_size-1) {
+                    escape_size += add*2;
+
+                    char *tmp_buffer = realloc(escaped_text, escape_size * sizeof(char));
+                    if (tmp_buffer != NULL) {
+                        escaped_text = tmp_buffer;
+                    }
+                    else {
+                        printf("Unable to allocate memory for tmp_buffer\n");
+                        exit(-1);
+                    }
+                }
+
+                strcat(escaped_text, to_add);
+                escaped_index += add;
+            }
+            else {
+                escaped_text[escaped_index++] = *to_add;
+            }
+
+            index++;
+        }
+
+        return escaped_text;
+    }
+
+    return NULL;
+
+}
