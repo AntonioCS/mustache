08fd2c039d59228c57b1847778241be24fd53e6c
antonio <a.simoes@allsaints.com>
2014-09-19 17:42:03 +0100

    Added a few test cases

--- main.c
+++ main.c
@@ -10,19 +10,28 @@
 #include <string.h>
 #include "src/mustache.h"
 
+char text_tag_variable[] = "Hello {{name}}";
+char text_tag_variable_no_escape[] = "Escaped html: {{html}} ---- Not escaped html: {{{html}}}";
+
+/*
 char text[] = "Hello {{name}}\n"
-               "You have just won {{value}} dollars {{{value}}}!\n"
+               "You have just won {{value}} dollars!\n"
+               "Escaped html: {{html}} ---- Not escaped html: {{{html}}}!\n"
                "{{#in_ca}}"
                "Well, {{taxed_value}} dollars, after taxes.\n"
                "{{/in_ca}}}"
         ;
-
+*/
 int main(int argc, char** argv) {
 
     pmustache m = mustache_init();
-
+    
+    //char *text = text_tag_variable;
+    char *text = text_tag_variable_no_escape;
+    
     mustache_set(m, "name", "António");
     mustache_set(m, "value", "123.33");
+    mustache_set(m, "html", "<script type=\"text/javascript\" src=\"acs.js\" />");
 
     mustache_load_txt(m, text);
 
