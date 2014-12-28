9211da619be729304f0cdf5398876904bc584e45
antoniocs <antoniocs@gmail.com>
2014-12-28 23:53:56 +0000

    Test header

---
+++ tests/minunit.h
@@ -0,0 +1,33 @@
+/* 
+ * File:   minunit.h
+ * http://www.jera.com/techinfo/jtns/jtn002.html
+ *
+ */
+
+#ifndef MINUNIT_H
+#define	MINUNIT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define mu_assert(message, test) do { \
+                                    if (!(test)) \
+                                        return message; \
+                                 } while (0)
+#define mu_run_test(test) do { \
+                            char *message = test(); \
+                            tests_run++; \
+                            if (message) \
+                                return message; \
+                          } while (0)
+    extern int tests_run;
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MINUNIT_H */
+
