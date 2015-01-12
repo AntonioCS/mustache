8cc2a5dddd8e1623af7c93dcadb2e08f18be7307
antoniocs <antoniocs@gmail.com>
2015-01-12 09:02:12 +0000

    Added test_text_get_char and converted to linux line endings

--- tests/test_text.c
+++ tests/test_text.c
@@ -4,16 +4,43 @@
 #include "minunit.h"
 
 int tests_run = 0;
+int asserts_run = 0;
 
 #include "../src/position.c"
 #include "../src/text.c"
 
 static pmustache m;
 
-static char *test_text_get_size() {
-    m->text_size = 10;    
+static char *test_text_get_char() {
+    m->text = "Hello";
+    m->text_size = 5;
+    m->text_position = 0;
+        
+    char expected_char = 'H';
     
-    mu_assert("error, text_get_size incorrect size", text_get_size(m) == 10);    
+    mu_assert("error, test_text_get_char did not return correct char", text_get_char(m) == expected_char);
+    return 0;
+}
+
+static char *test_text_get_char_pos() {    
+    m->text = "Hello";
+    m->text_size = 5;
+    
+    char expected_char = 'e';
+    int position = 1;
+    
+    mu_assert("error, text_get_char_pos did not return correct char", text_get_char_pos(m, position) == expected_char);
+    
+    //out side limit
+    position = 20;
+    mu_assert("error, text_get_char_pos did not return EOF", text_get_char_pos(m, position) == EOF);
+    return 0;
+}
+
+static char *test_text_set() {    
+    text_set(m, "Hello");
+            
+    mu_assert("error, text_set did not set the text correctly", (strcmp(m->text, "Hello") == 0));
     return 0;
 }
 
@@ -25,28 +52,22 @@ static char *test_text_set_size() {
     return 0;
 }
 
-static char *test_text_set() {    
-    text_set(m, "Hello");
-            
-    mu_assert("error, text_set did not set the text correctly", (strcmp(m->text, "Hello") == 0));
+static char *test_text_get_size() {
+    m->text_size = 10;    
+    
+    mu_assert("error, text_get_size incorrect size", text_get_size(m) == 10);    
     return 0;
 }
 
-static char *test_text_get_char_pos() {    
-    m->text = "Hello";
-    m->text_size = 5;
-            
-    mu_assert("error, text_get_char_pos did not return correct char", text_get_char_pos(m, 1) == 'e');
-    return 0;
-}
 
 static char *all_tests() {
     m = malloc(sizeof (mustache));    
 
-    mu_run_test(test_text_get_size);
-    mu_run_test(test_text_set_size);
-    mu_run_test(test_text_set);
+    mu_run_test(test_text_get_char);
     mu_run_test(test_text_get_char_pos);
+    mu_run_test(test_text_set);    
+    mu_run_test(test_text_set_size);
+    mu_run_test(test_text_get_size);
     
     return 0;
 }
@@ -58,7 +79,7 @@ int main(int argc, char** argv) {
     } else {
         printf("ALL TESTS PASSED\n");
     }
-    printf("Tests run: %d\n", tests_run);
+    printf("Tests run: %d\nAsserts executed: %d\n", tests_run, asserts_run);
 
     return result != 0;
 }
