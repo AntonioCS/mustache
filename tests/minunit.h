1ae58e1ad22946dd67e0c45609ceb2ec707dd69b
antoniocs <antoniocs@gmail.com>
2015-02-06 08:27:44 +0000

    Fixed line breaks. Added mu_main that defines the main structure for the test file

--- tests/minunit.h
+++ tests/minunit.h
@@ -13,17 +13,31 @@ extern "C" {
 
 #define mu_assert(message, test) do { \
                                     asserts_run++; \
-                                    if (!(test)) \
+                                    if (!(test)) {\
                                         return message; \
+                                    } \
                                  } while (0)
 #define mu_run_test(test) do { \
                             char *message = test(); \
                             tests_run++; \
-                            if (message) \
+                            if (message) { \
                                 return message; \
+                            } \
                           } while (0)
     extern int tests_run;
     extern int asserts_run;
+
+#define mu_main int main(int argc, char** argv) { \
+    char *result = all_tests(); \
+    if (result != 0) { \
+        printf("%s\n", result); \
+    } else { \
+        printf("ALL TESTS PASSED\n"); \
+    } \
+    printf("Tests run: %d\n", tests_run); \
+ \
+    return result != 0; \
+} \
 
 
 
