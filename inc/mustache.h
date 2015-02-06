8a84e2aba1b35ebc1b1130fdac185a41bf07309f
antoniocs <antoniocs@gmail.com>
2015-02-06 08:41:45 +0000

    Fixed function pointers. Named main struct

--- inc/mustache.h
+++ inc/mustache.h
@@ -31,7 +31,7 @@ extern "C" {
 #define MUSTACHE_TAGS_TYPE_DELIMITER '='
 #define MUSTACHE_TAGS_PARTIAL_EXT "mustache"   
 
-    typedef struct {
+    typedef struct mustache {
         char *text;
         size_t text_position;
         size_t text_size;
@@ -61,8 +61,8 @@ extern "C" {
         
         
         //source text function
-        char (*text_get_char)(pmustache);
-        char (*text_get_char_pos)(pmustache, int);
+        char *(*text_get_char)(struct mustache *);
+        char *(*text_get_char_pos)(struct mustache *, int);
         
         
     } mustache, *pmustache;
